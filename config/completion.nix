957e556d786c346e434f76f71a0f97981939f817
Luna <chloe@transgender.army>
2024-02-21 16:40:10 +0100

    test

---
+++ config/completion.nix
@@ -0,0 +1,24 @@
+{pkgs, ...}: 
+{
+  plugins = {
+    
+    cmp-nvim-lsp.enable = true;
+    cmp-path.enable = true;
+    cmp-git.enable = true;
+    cmp-buffer.enable= true;
+    cmp-calc.enable = true;
+    cmp-cmdline.enable = true;
+    cmp_luasnip.enable = true;  
+    
+    luasnip.enable = true;
+    nvim-cmp.enable = true;
+
+    lsp = {
+      enable = true;
+      servers = {
+        nixd.enable = true;
+      };
+    };
+  };
+
+}
