04710816a1a042f47c02a1c87e79a5a2c7a0e04f
Luna <chloe@transgender.army>
2024-02-26 16:03:16 +0100

    mrow

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -4,7 +4,7 @@
   plugins.which-key.triggers = [ "<leader>" "g" ];
   plugins.which-key.registrations = {
     "<leader>t" = [ "<cmd>Neotree toggle<cr>" "toggle Neotree" ];
-    "<leader>-<Tab>" = ["<cmd>tabr<cr>" "previous Tab"];
+    "<leader>-<Tab>" = ["<cmd>BufferLast<cr>" "previous Tab"];
     "<leader>w" = [ "<cmd>w!<cr>" "Write current buffer" ];
     "<leader>wq" = [ "<cmd>wq<cr>" "Write current buffer and quit" ];
     "<leader>wqq" = [ "<cmd>wq<cr>" "Write & quit forced" ];
@@ -12,7 +12,10 @@
     "<c-j>" = [ "<c-w>j" "Move down" ];
     "<c-k>" = [ "<c-w>k" "Move left" ];
     "<c-l>" = [ "<c-w>l" "Move right" ];
-    "<Tab>" = ["<cmd>tabN<CR>" "next tab"];
-    "<S-Tab>" = ["<cmd>tabp<CR>" "previous tab"];
+    "<Tab>" = ["<cmd>BufferNext<CR>" "next tab"];
+    "<S-Tab>" = ["<cmd>BufferPrevious<CR>" "previous tab"];
+    "<c><Tab>" = ["<cmd>BufferNext<CR>" "next tab"];
+    "<c><S-Tab>" = ["<cmd>BufferPrevious<CR>" "previous tab"];
+     
   };
 }
