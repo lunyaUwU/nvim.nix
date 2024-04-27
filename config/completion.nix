6efcdf73abb0c19e318f9e1f811d5390d2d25a00
Luna <chloe@transgender.army>
2024-04-27 19:43:03 +0200

    meow

--- config/completion.nix
+++ config/completion.nix
@@ -15,8 +15,19 @@
        
       };
     };
-    cmp.enable = true;
-  };
+    cmp = {
+      enable = true;
+      settings.snippet.expand =
+      ''
+      function(args)
+        require('luasnip').lsp_expand(args.body)
+      end
+      '';
+
+
+      };
+    };  
+    };
 
 }
 
