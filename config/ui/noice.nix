48cb9b9fb69c4c76a64525c560245886001eb9a6
Luna Arson <luna@Lunas-MacBook-Air.local>
2026-09-03 00:07:28 +0200

    meow

--- config/ui/noice.nix
+++ config/ui/noice.nix
@@ -2,32 +2,59 @@
   plugins.noice = {
     enable = true;
     settings = {
-    notify = {
-      enabled = false;
-    };
-    messages = {
-      enabled = true; # Adds a padding-bottom to neovim statusline when set to false for some reason
-    };
-    lsp = {
-      message = {
+     routes = [
+        {
+          filter = {
+            event = "msg_show";
+            kind = "shell_out";
+          };
+          view = "notify";
+          opts = {
+            enter = true;
+          };
+        }
+        {
+          filter = {
+            event = "msg_show";
+            kind = "shell_err";
+          };
+          view = "notify";
+          opts = {
+            enter = true;
+          };
+        }
+      ];
+      notify = {
         enabled = true;
+        view = "notify";
       };
-      progress = {
-        enabled = false;
-        view = "mini";
+      messages = {
+        enabled = true; # Adds a padding-bottom to neovim statusline when set to false for some reason
+      };
+      lsp = {
+        message = {
+          enabled = true;
+        };
+        progress = {
+          enabled = false;
+          view = "mini";
+        };
+      };
+      cmdline.enabled = true;
+      popupmenu = {
+        enabled = true;
+        backend = "nui";
+      };
+      format = {
+        filter = {
+          title = "Shell";
+        };
+        replace = {
+          pattern = [":%s*%%s*s:%w*:%s*" ":%s*%%s*s!%w*!%s*" ":%s*%%s*s/%w*/%s*" "%s*s:%w*:%s*" ":%s*s!%w*!%s*" ":%s*s/%w*/%s*"];
+          icon = "󱞪";
+          lang = "regex";
+        };
       };
     };
-    popupmenu = {
-      enabled = true;
-      backend = "nui";
-    };
-    format = {
-      replace = {
-        pattern = [":%s*%%s*s:%w*:%s*" ":%s*%%s*s!%w*!%s*" ":%s*%%s*s/%w*/%s*" "%s*s:%w*:%s*" ":%s*s!%w*!%s*" ":%s*s/%w*/%s*"];
-        icon = "󱞪";
-        lang = "regex";
-      };
-    };
-  };
   };
 }
